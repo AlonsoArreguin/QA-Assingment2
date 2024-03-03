70287e4e96502f81df8c2b2af8fe82cf3ff2a2a2
Alonso Arreguin <alonsoarreguin2001@gmail.com>
2024-03-03 16:17:54 -0600

    Main
    Added Main to project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float weight;
+    cout << "What is your wight in lbs?" << endl;
+    cin >> weight;
+    cout << "This is your weight: " << weight << endl;
+    return 0; 
+}
